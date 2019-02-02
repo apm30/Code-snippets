e1f2232e47e9b810c479dce757545198bf018937
Domo <stolarczyk.dominik@gmail.com>
2019-02-02 15:58:29 +0100

    Reactive Forms

---
+++ reactive-forms/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { SignupFormComponent } from './signup-form/signup-form.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
