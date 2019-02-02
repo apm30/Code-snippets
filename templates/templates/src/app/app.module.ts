8effc26c0a444ed4cfe7cc0299fe5ae8adc1f0de
Domo <stolarczyk.dominik@gmail.com>
2019-02-02 10:49:15 +0100

    Basic forms

---
+++ templates/templates/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+
+@NgModule({
+  declarations: [AppComponent, ContactFormComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, // <========== Add this line!
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
